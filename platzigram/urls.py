196a9ec0d82f26ae12537d72d12b067b9ec6738d
Eduardo Imery <ejwimery@gmail.com>
2019-08-15 23:32:44 -0600

    Finishin hello-corders greeting

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,10 +1,14 @@
+""" """
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-urlpatterns = [
-    path('hello-world',)
-]
-#    path('admin/', admin.site.urls),
 
 def hello_world(request):
+    return HttpResponse('Hello coders!')
+
+
+urlpatterns = [
+    path('hello-world/', hello_world)
+]
+#    path('admin/', admin.site.urls),
