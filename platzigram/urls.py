bec2aa4e5ead60398297b52c2e18d8f6fb567d3a
Eduardo Imery <ejwimery@gmail.com>
2019-08-17 09:07:57 -0600

    creating views file

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,10 @@
-""" """
+""" Platzi URL"""
 from django.contrib import admin
 from django.urls import path
-
+from platzigram import views
 
 urlpatterns = [
-    path('hello-world/', hello_world)
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 ]
 #    path('admin/', admin.site.urls),
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,5 +1,14 @@
+""" Platzigram views"""
+# Django
 from django.http import HttpResponse
+# Utilities
+from datetime import datetime
 
 
 def hello_world(request):
-    return HttpResponse('Hello coders!')
+    now = str(datetime.now().strftime('%b %dth, %Y - %H:%M hrs'))
+    return HttpResponse('Hello coders! current time server is {}'.format(now))
+
+
+def hi(request):
+    return HttpResponse('Hi!')
