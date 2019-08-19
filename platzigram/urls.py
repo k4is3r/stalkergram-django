01d6e32a249577e03db40f168da08502ab5f25aa
Eduardo Imery <ejwimery@gmail.com>
2019-08-19 09:33:50 -0600

    before delete everything

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,6 +1,9 @@
 """ Platzi URL"""
+#Django
 from django.contrib import admin
 from django.urls import path
+
+#Local
 from platzigram import views as local_views
 from posts import views as posts_views
 
@@ -8,7 +11,7 @@ urlpatterns = [
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sorted_int),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
-
     path('posts/', posts_views.list_posts),
+    path('admin/', admin.site.urls),
 ]
 #    path('admin/', admin.site.urls),
--- posts/models.py
+++ posts/models.py
@@ -17,3 +17,7 @@ class User(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+ 
