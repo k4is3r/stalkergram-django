d522910df4416b32a00b21e9f23a7ac4135e2fc3
Eduardo Imery <ejwimery@gmail.com>
2019-08-19 16:36:17 -0600

    implementing auth & validation, login & logout

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
     path('users/login/', users_views.login_view,name='login'),
     path('posts/', posts_views.list_posts, name='feed'),
+    path('users/logout',users_views.logout_view,name='logout'),
+    path('users/signup', users_views.signup, name='signup'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect 
-
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from users.models import Profile
+from django.db.utils import IntegrityError
 
 def login_view(request):
     if request.method == 'POST':
@@ -17,3 +20,30 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error':'Invalid Username and Password'})
     return render(request, 'users/login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error':'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username = username,password = password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error':'Username invalid try another one'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
