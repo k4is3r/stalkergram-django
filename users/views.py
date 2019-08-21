ab47c461704af59995c71960502c42f592399483
Eduardo Imery <ejwimery@gmail.com>
2019-08-21 01:42:20 -0600

    fixing validations with django forms

--- users/views.py
+++ users/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.models import User
-from users.models import Profile
-from django.db.utils import IntegrityError
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 def login_view(request):
@@ -26,24 +23,17 @@ def login_view(request):
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirmation = request.POST['password_confirmation']
-        if password != password_confirmation:
-            return render(request, 'users/signup.html', {'error':'Password confirmation does not match'})
-        try:
-            user = User.objects.create_user(username = username,password = password)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error':'Username invalid try another one'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-        profile = Profile(user=user)
-        profile.save()
-        return redirect('login')
-    return render(request, 'users/signup.html')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = SignupForm()
+            return render(
+                request = request,
+                template_name='users/signup.html',
+                context={'form' : form}
+            )
 
 @login_required
 def update_profile(request):
