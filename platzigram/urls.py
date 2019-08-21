2ff56ccca42b720ab938fabfd2c9c5d4389fff19
Eduardo Imery <ejwimery@gmail.com>
2019-08-21 17:59:55 -0600

    remove example views from project and methods in url filw

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -3,17 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from platzigram import views as local_views
 from posts import views as posts_views
 from users import views as users_views
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-
-    path('hello-world/', local_views.hello_world, name='hello_world'),
-    path('sorted/', local_views.sorted_integers, name='sorted'),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
     path('users/login/', users_views.login_view,name='login'),
     path('', posts_views.list_posts, name='feed'),
     path('posts/new/', posts_views.create_post, name='create_post'),
